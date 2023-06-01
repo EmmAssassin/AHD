d6362f36db5ac5ad1c44ce4b765c6a033a487d56
EmmAssassin <emmassassin@gmail.com>
2023-06-01 20:26:47 +1000

    updates

--- AHD/assets/scripts/navigationScript.js
+++ AHD/assets/scripts/navigationScript.js
@@ -10,6 +10,7 @@ function startUp() {
         if($(window).width() > 1024){   
             if($('.collapsable').hasClass('open')) {
                 $('.collapsable').removeClass('open');
+                $('main').removeClass('no-move');   
             }            
                    
             $('.topLine').removeClass('rightCross');            
@@ -35,7 +36,8 @@ function startUp() {
 
 function navFunction(){
  
-        $('.collapsable').toggleClass('open');    
+        $('.collapsable').toggleClass('open'); 
+        $('main').toggleClass('no-move');   
         $('.topLine').toggleClass('rightCross');      
         $('.midLine').toggleClass('hidden');     
         $('.bottomLine').toggleClass('leftCross'); 
@@ -53,6 +55,7 @@ function openStates(value) {
     var html = "";
 
     if(value == 1){
+        /*
         html += '<li id="states"><a href="./assets/pages/canberra.html">Australian Capital Territory</a></li>' + 
         '<li id="states"><a href="./assets/pages/newSouthWales.html">New South Wales</a></li>' +
         '<li id="states"><a href="./assets/pages/northernTerritory.html">Northern Territory</a></li>' +
@@ -60,9 +63,19 @@ function openStates(value) {
         '<li id="states"><a href="./assets/pages/southAus.html">South Australia</a></li>' +
         '<li id="states"><a href="./assets/pages/tasmania.html">Tasmania</a></li>' +
         '<li id="states"><a href="./assets/pages/victoria.html">Victoria</a></li>' +
-        '<li id="states"><a href="./assets/pages/westernAus.html">Western Australia</a></li>';    
+        '<li id="states"><a href="./assets/pages/westernAus.html">Western Australia</a></li>';   
+        */ 
+        html += '<li id="states"><button value="ACT" onclick="statesLoad(this.value)">Australian Capital Territory</button></li>' + 
+        '<li id="states"><button value="NSW" onclick="statesLoad(this.value)">New South Wales</button></li>' +
+        '<li id="states"><button value="NT" onclick="statesLoad(this.value)">Northern Territory</button></li>' +
+        '<li id="states">button value="QLD" onclick="statesLoad(this.value)">Queensland</a></li>' + 
+        '<li id="states"><button value="SA" onclick="statesLoad(this.value)">South Australia</button></li>' +
+        '<li id="states"><button value="TAS" onclick="statesLoad(this.value)">Tasmania</button></li>' +
+        '<li id="states"><button value="VIC" onclick="statesLoad(this.value)">Victoria</button></li>' +
+        '<li id="states"><button value="WA" onclick="statesLoad(this.value)">Western Australia</button></li>';
     }
     else {
+        /*
         html += '<li id="states"><a href="./canberra.html">Australian Capital Territory</a></li>' + 
     '<li id="states"><a href="./newSouthWales.html">New South Wales</a></li>' +
     '<li id="states"><a href="./northernTerritory.html">Northern Territory</a></li>' +
@@ -71,6 +84,15 @@ function openStates(value) {
     '<li id="states"><a href="./tasmania.html">Tasmania</a></li>' +
     '<li id="states"><a href="./victoria.html">Victoria</a></li>' +
     '<li id="states"><a href="./westernAus.html">Western Australia</a></li>';
+    */
+    html += '<li id="states"><button value="ACT" onclick="statesLoad(this.value)">Australian Capital Territory</button></li>' + 
+    '<li id="states"><button value="NSW" onclick="statesLoad(this.value)">New South Wales</button></li>' +
+    '<li id="states"><button value="NT" onclick="statesLoad(this.value)">Northern Territory</button></li>' +
+    '<li id="states">button value="QLD" onclick="statesLoad(this.value)">Queensland</a></li>' + 
+    '<li id="states"><button value="SA" onclick="statesLoad(this.value)">South Australia</button></li>' +
+    '<li id="states"><button value="TAS" onclick="statesLoad(this.value)">Tasmania</button></li>' +
+    '<li id="states"><button value="VIC" onclick="statesLoad(this.value)">Victoria</button></li>' +
+    '<li id="states"><button value="WA" onclick="statesLoad(this.value)">Western Australia</button></li>';
     }
     
     $('.collapsableStates').toggleClass('hide');
