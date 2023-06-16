e5e3193a61542e4be31a8efe2eb70b98ea711105
EmmAssassin <emmassassin@gmail.com>
2023-06-16 10:16:27 +1000

    Table fixes

--- AHD/assets/scripts/navigationScript.js
+++ AHD/assets/scripts/navigationScript.js
@@ -53,25 +53,27 @@ function navFunction(){
 
 function openStates(value) {
     var html = "";
-
-        /*
-        html += '<li id="states"><a href="./assets/pages/canberra.html">Australian Capital Territory</a></li>' + 
-        '<li id="states"><a href="./assets/pages/newSouthWales.html">New South Wales</a></li>' +
-        '<li id="states"><a href="./assets/pages/northernTerritory.html">Northern Territory</a></li>' +
-        '<li id="states"><a href="./assets/pages/queensland.html">Queensland</a></li>' + 
-        '<li id="states"><a href="./assets/pages/southAus.html">South Australia</a></li>' +
-        '<li id="states"><a href="./assets/pages/tasmania.html">Tasmania</a></li>' +
-        '<li id="states"><a href="./assets/pages/victoria.html">Victoria</a></li>' +
-        '<li id="states"><a href="./assets/pages/westernAus.html">Western Australia</a></li>';   
-        */ 
-    html += '<li id="states"><button value="canberra" onclick="statesLoad(this.value)">Australian Capital Territory</button></li>' + 
-    '<li id="states"><button value="newSouthWales" onclick="statesLoad(this.value)">New South Wales</button></li>' +
-    '<li id="states"><button value="northernTerritory" onclick="statesLoad(this.value)">Northern Territory</button></li>' +
-    '<li id="states"><button value="queensland" onclick="statesLoad(this.value)">Queensland</a></li>' + 
-    '<li id="states"><button value="southAus" onclick="statesLoad(this.value)">South Australia</button></li>' +
-    '<li id="states"><button value="tasmania" onclick="statesLoad(this.value)">Tasmania</button></li>' +
-    '<li id="states"><button value="victoria" onclick="statesLoad(this.value)">Victoria</button></li>' +
-    '<li id="states"><button value="westernAus" onclick="statesLoad(this.value)">Western Australia</button></li>';
+    
+        if(value == 1){
+            html += '<li id="states"><button value="canberra-HOME" onclick="statesLoad(this.value)">Australian Capital Territory</button></li>' + 
+            '<li id="states"><button value="newSouthWales-HOME" onclick="statesLoad(this.value)">New South Wales</button></li>' +
+            '<li id="states"><button value="northernTerritory-HOME" onclick="statesLoad(this.value)">Northern Territory</button></li>' +
+            '<li id="states"><button value="queensland-HOME" onclick="statesLoad(this.value)">Queensland</button></li>' + 
+            '<li id="states"><button value="southAus-HOME" onclick="statesLoad(this.value)">South Australia</button></li>' +
+            '<li id="states"><button value="tasmania-HOME" onclick="statesLoad(this.value)">Tasmania</button></li>' +
+            '<li id="states"><button value="victoria-HOME" onclick="statesLoad(this.value)">Victoria</button></li>' +
+            '<li id="states"><button value="westernAus-HOME" onclick="statesLoad(this.value)">Western Australia</button></li>';    
+        }
+        else {
+            html += '<li id="states"><button value="canberra" onclick="statesLoad(this.value)">Australian Capital Territory</button></li>' + 
+        '<li id="states"><button value="newSouthWales" onclick="statesLoad(this.value)">New South Wales</button></li>' +
+        '<li id="states"><button value="northernTerritory" onclick="statesLoad(this.value)">Northern Territory</button></li>' +
+        '<li id="states"><button value="queensland" onclick="statesLoad(this.value)">Queensland</button></li>' + 
+        '<li id="states"><button value="southAus" onclick="statesLoad(this.value)">South Australia</button></li>' +
+        '<li id="states"><button value="tasmania" onclick="statesLoad(this.value)">Tasmania</button></li>' +
+        '<li id="states"><button value="victoria" onclick="statesLoad(this.value)">Victoria</button></li>' +
+        '<li id="states"><button value="westernAus" onclick="statesLoad(this.value)">Western Australia</button></li>';
+        }
     
     $('.collapsableStates').toggleClass('hide');
 
@@ -93,31 +95,42 @@ function openStates(value) {
 }
 
 function statesLoad(state) {
-    if (state == "ACT") { 
-        var sendArray = [{stateName: "Australian Capital Territory", acronym: "ACT"}];
+    var states = state.split('-');
+    console.log(states[0]);
+    console.log(states[1]);
+
+    if (states[0] == "canberra") { 
+        var sendArray = ["Australian Capital Territory", "ACT"];  
     }
-    else if (state == "NSW"){
-        var sendArray = [{stateName: "New South Wales", acronym: "NSW"}];
+    else if (states[0] == "newSouthWales"){
+        var sendArray = ["New South Wales", "NSW"];
     }
-    else if (state == "NT"){
-        var sendArray = [{stateName: "Northern Territory", acronym: "NT"}];
+    else if (states[0] == "northernTerritory"){
+        var sendArray = ["Northern Territory", "NT"];
     }
-    else if (state == "QLD"){
-        var sendArray = [{stateName: "Queensland", acronym: "QLD"}];
+    else if (states[0] == "queensland"){
+        var sendArray = ["Queensland", "QLD"];
     }
-    else if (state == "SA"){
-        var sendArray = [{stateName: "South Australia", acronym: "SA"}];
+    else if (states[0] == "southAus"){
+        var sendArray = ["South Australia", "SA"];
     }
-    else if (state == "TAS"){
-        var sendArray = [{stateName: "Tasmania", acronym: "TAS"}];
+    else if (states[0] == "tasmania"){
+        var sendArray = ["Tasmania", "TAS"];
     }
-    else if (state == "VIC"){
-        var sendArray = [{stateName: "Victoria", acronym: "VIC"}];
+    else if (states[0] == "victoria"){
+        var sendArray = ["Victoria", "VIC"];
     }
-    else if (state == "WA"){
-        var sendArray = [{stateName: "Western Australia", acronym: "WA"}];
+    else if (states[0] == "westernAus"){
+        var sendArray = ["Western Australia", "WA"];
     }
 
-    window.location.href = state + ".html";
-    localStorage.setItem('stateArray', sendArray);
+    if(states[1] != undefined){
+        window.location.href = "./assets/pages/" + states[0] + ".html";
+    }
+    else{
+        window.location.href = states[0] + ".html";
+    }
+    var bob = "hi";
+    sessionStorage.setItem('thisArray', bob);
+    sessionStorage.setItem('stateArray', sendArray);
 }
--- AHD/assets/scripts/tableScript.js
+++ AHD/assets/scripts/tableScript.js
@@ -1,9 +1,127 @@
 
 $(function () {
-    var stateArray = localStorage.getItem('stateArray');
+    const stateArray = sessionStorage.getItem('stateArray');
+    const thisA = sessionStorage.getItem('thisArray');
     buildTable(stateArray);
 })
 
 function buildTable(stateArray){
-    console.log(stateArray.stateName);
+    console.log(stateArray);
+    var tablehtml = "";
+    var headinghtml = "";
+    
+    var stateTable = stateArray.split(',');
+    console.log(stateTable);
+     for(i=0; i<stateTable.length;i++){
+        console.log(stateTable[i]);
+     }
+
+     const table = document.createElement("table");
+
+     headinghtml += '<h1 id="pageHeading">' + stateTable[0] + '</h1>';
+
+     $('main').append(headinghtml);
+     $('main').append(table);
+     //$('table').addClass('dataTable');
+     
+     tablehtml += '<table class="dataTable"><thead><tr><th id="hideMe" rowspan="2"></th><th rowspan="2" id="bold">Available dates</th><th colspan="6" id="bold">Age Interval x Year Interval</th></tr>' +
+     '<tr><th id="boldTwo">1x1</th><th id="boldTwo">1x5</th><th id="boldTwo">1x10</th><th id="boldTwo">5x1</th><th id="boldTwo">5x5</th><th id="boldTwo">5x10</th></tr></thead>' +
+     '<tbody><tr>' + 
+     '<td>Births</td><td>1971-2016</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Births.txt">1x1</a></td>' +
+     '<td id="clickables"></td><td id="clickables"></td><td id="clickables"></td><td id="clickables"></td><td id="clickables"></td>' + 
+     '</tr><tr>' + 
+     '<td>Deaths</td><td>1971-2016</td>' + 
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Deaths_5x10.txt"> 5x10 </a></td>' +
+     '</tr><tr>' +
+     '<td>Population Size</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Population.txt"> 1x1 </a></td>' +
+     '<td id="clickables"></td>' +
+     '<td id="clickables"></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Population5.txt"> 5x1 </a></td>' +
+     '<td id="clickables"></td>' +
+     '<td id="clickables"></td>' +
+     '</tr><tr>' +
+     '<td>Exposure-to-Risk</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Exposures_5x10.txt"> 5x10 </a></td>' +
+     '</tr><tr>' +
+     '<td>Death Rates</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/Mx_5x10.txt"> 5x10 </a></td>' +
+     '</tr><tr>' +
+     '<td>Life Tables - Female</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/fltper_5x10.txt"> 5x10 </a></td>'+
+     '</tr><tr>' +
+     '<td>Life Tables - Male</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/mltper_5x10.txt"> 5x10 </a></td>' +
+     '</tr><tr>' +
+     '<td>Life Tables - Total</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_1x1.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_5x1.txt"> 5x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_5x5.txt"> 5x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/bltper_5x10.txt"> 5x10 </a></td>' +
+     '</tr><tr>' +
+     '<td>Life Expectancy at Birth</td><td>dates</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/E0per.txt"> 1x1 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/E0per_1x5.txt"> 1x5 </a></td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/E0per_1x10.txt"> 1x10 </a></td>' +
+     '<td id="clickables"></td>' +
+     '<td id="clickables"></td>' +
+     '<td id="clickables"></td>' +
+     '</tr></tbody></table>' +
+     '<h2>Input Data</h2>' +
+     '<table class="dataTable"><thead><tr><th id="hideMe" rowspan="2"></th><th rowspan="2" id="bold">Available dates</th><th  id="bold">Data</th><th id="bold">Lexis map</th></tr></thead>' +
+     '<tbody><tr>' +
+     '<td>Births</td><td>1971-2016</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/InputDB/' + stateTable[1] + 'birth.txt"> txt </a></td> <td id="clickables"></td>' +
+     '</tr><tr>' +
+     '<td>Deaths</td><td>1971-2016</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/InputDB/' + stateTable[1] + 'death.txt"> txt </a></td> <td id="clickables"><a></a></td>' +
+     '</tr><tr>' +
+     '<td>Population size</td><td>1971-2016</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/InputDB/' + stateTable[1] + 'pop.txt"> txt </a></td> <td id="clickables"><a></a></td>' +
+     '</tr><tr>' +
+     '<td>Reference file</td><td>1971-2016</td>' +
+     '<td id="clickables"><a href = "../txtFiles/humanMortality/' + stateTable[1] + '/InputDB/' + stateTable[1] + 'ref.txt"> txt </a></td> <td id="clickables"><a></a></td>' +
+     '</tr></tbody></table>';
+
+     $('main').append(tablehtml);
+     
 }
+
+
+
+
+
+
+
+
+
+
+
